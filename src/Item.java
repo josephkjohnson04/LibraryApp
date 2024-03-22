7f4a0040914b9d5fb7c4fcaf5bad55181e6cb01d
kyhans07 <kyhans07@wsc.edu>
2024-03-22 09:45:39 -0500

    classes + ID

--- src/Item.java
+++ src/Item.java
@@ -4,18 +4,19 @@ import java.util.Date;
 
 public class Item {
 
+    private static int lastNumber = 0;
+    private int ItemID;
+    private String title;
+    private String desc;
+    private Date invDate;
 
-private int ItemID;
-private String title;
-private String desc;
-private Date invDate;
+    public Item() {
 
+        this.ItemID = ++Item.lastNumber;
 
-
+    }
 
 }
-
-
 
 
 

b2ab735077de615a9ee48ac1ca5fb023abda5101
caleb-jenkinson15 <caleb.jenkinson15@gmail.com>
2024-04-01 17:38:46 -0500

    Added the menu into the LibraryApp and modified book inside of the menu

---
+++ src/Input.java
@@ -0,0 +1,158 @@
+// package library.app;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+/**
+ * Handles all user's input with validation
+ * 
+ * @author dejohns2
+ * @since 2024.03.27
+ * @version 1.0 beta
+ */
+public class Input {
+	
+    /**
+     * Scanner used for all user's input
+     */
+    public static Scanner sc = new Scanner(System.in);
+
+    /**
+     * Display a prompt and return the entire user input line
+     * 
+     * @param prompt input prompt asking the user to enter something specific
+     * @return the user's input line 
+     */
+    public static String getLine(String prompt) {
+		
+		System.out.print(prompt);
+		return Input.sc.nextLine();
+		
+	} // end of getLine
+
+    /**
+     * Display a prompt and return the entire user input line, not allowing for an empty string
+     *
+     * @param prompt input prompt asking the user to enter something specific
+     * @return the user's input line
+     */
+    public static String getString(String prompt) {
+
+        String userInput = null;
+
+        System.out.print(prompt);
+
+        while (true) {
+            userInput = Input.sc.nextLine();
+
+            userInput = userInput.trim();
+
+            if (userInput.isEmpty()){
+                System.out.print("Invalid input! Please enter a value: ");
+            } else {
+                break;
+            }
+        }
+
+        return userInput;
+
+    } // end of getLine
+
+    /**
+     * Display a prompt and validate that the user entered a valid integer
+     * and then return the valid value
+     * 
+     * @param prompt input prompt asking the user to enter something specific
+     * @return the valid user's integer input
+     */
+    public static int getInt(String prompt) {
+    	int userInput = 0;
+		
+		System.out.print(prompt);
+		
+		while (true) {
+			if (Input.sc.hasNextInt()) // if the data in the buffer is a valid integer
+				break;           // then break out of the validation loop
+			
+			System.out.printf("Invalid input! Please enter a number: ");
+			Input.sc.next();  // clear the data in the input buffer
+			
+		} // end of while
+			
+		userInput = Input.sc.nextInt();
+		Input.sc.nextLine(); // consume newline left-over
+		
+		return userInput;
+		
+	} // end of getInt
+    
+    /**
+     * Display a prompt and validate that the user entered a valid integer
+     * based on a low and high range inclusively
+     * and then return the valid value
+     * 
+     * @param prompt input prompt asking the user to enter something specific
+     * @param low the lowest valid value inclusively
+     * @param high the highest valid value inclusively
+     * @return the valid user's integer input
+     */
+    public static int getIntRange(String prompt, int low, int high) {
+    	int userInput;
+		
+		System.out.print(prompt);
+		
+		while (true) {
+			if (Input.sc.hasNextInt()) {         // if the data in the buffer is a valid integer
+				userInput = Input.sc.nextInt();  // then store the integer in userInput
+				
+				// if the userInput is within the valid range
+				// then break out of the validation loop
+				if (userInput >= low && userInput <= high) {
+					break;
+				} // end of if
+
+    		} else {
+    			Input.sc.next();  // clear the data in the input buffer
+    		} // end of if-else
+
+			System.out.printf("Invalid input! Please enter a number between (%d - %d): ", low, high);
+
+		} // end of while
+		
+		Input.sc.nextLine(); // consume newline left-over
+
+		return userInput;
+		
+	} // end of getIntRange
+
+    /**
+     * Display a prompt and validate that the user entered a valid date MM-DD-YYYY
+     * and then return the valid value
+     *
+     * @param prompt input prompt asking the user to enter something specific
+     * @return the valid user's date input
+     */
+    public static String getDate(String prompt) {
+        String userInput = null;
+
+        System.out.print(prompt);
+
+        while (true) {
+
+            userInput = Input.sc.nextLine();
+
+            try{
+                LocalDate.parse(userInput, DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+                break;
+            } catch (Exception e){
+                System.out.print("Invalid input! Please enter a valid date (MM-DD-YYYY): ");
+            }
+        }
+
+        return userInput;
+
+    } // end of getInt
+
+
+} // end of library.app.Input class
--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,34 +1,226 @@
+
+//package library.app;
+
+// import library.inventory.Book;
+// import library.inventory.BookGenre;
+// import library.inventory.Item;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Section 4 - Library App
+ * Library App that allows adding, deleting, displaying, saving, and loading
+ inventory for Books, CDs, and DVDs.
+ * <a href="PUT_YOUR_URL_HERE">GitHub Repository URL</a>
  *
- * @author Caleb Jenkinson, Kyler Hanson, and Joe Johnson
- * @version 1.0
- * @link https://github.com/josephkjohnson04/LibraryApp.git
+ * @author PUT_YOUR_NAMES_HERE
+ * @since PUT_THE_CURRENT_DATE_HERE
+ * @version 1.0 beta
+ *
  */
 
 public class LibraryApp {
-    public static void main(String[] args) throws Exception {
-        try {
-            // testing for Item.java
-            //Item item1 = new Item("Book", "03-26-2024");
-            //System.out.println("toString: " + item1.toString());
-            //System.out.println("displayItem: ");
-            //item1.displayItem();
-            //item1.setDesc("Description");
-            //System.out.println("ID: " + item1.getId());
-            //System.out.println("Title: " + item1.getTitle());
-            //System.out.println("Description: " + item1.getDesc());
-            //System.out.println("Date: " + item1.getDateString());
-
-            // testing for book
-            Book book1 = new Book(BookGenre.Fiction, "03-28-2024", "Book1", "Bob");
-            System.out.println("Book genre: " + book1.getBookGenre());
-            System.out.println("Date: " + book1.getInvDate());
-            System.out.println("Title: " + book1.getTitle());
-            System.out.println("Author: " + book1.getAuthor());
-            book1.displayItem();
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+    /**
+     * Standard double dash line.
+     */
+    private final static String DOUBLE_LINE =
+            "==========================================================";
+    /**
+     * Standard single dash line.
+     */
+    private final static String SINGLE_LINE =
+            "---------------------------------------------------------";
+    /**
+     * List to store all types of inventory items (Book, Cd, Dvd).
+     */
+    private List<Item> inventory;
+    /**
+     * Default constructor instantiates the ArrayList for storing items.
+     */
+    public LibraryApp(){
+        this.inventory = new ArrayList<Item>();
+    } // end of constructor
+    /**
+     * Display the program's title.
+     */
+    private void displayAppHeading() {
+        System.out.println(DOUBLE_LINE);
+        System.out.println("Welcome to the Library App");
+        System.out.println(DOUBLE_LINE);
+    } // end of displayAppHeading method
+    /**
+     * Allows the user to enter an inventory id to be deleted.
+     */
+    private void deleteItem(){
+        int id = 0;
+        System.out.println("Delete Inventory");
+        System.out.println(SINGLE_LINE);
+        id = Input.getInt("Please enter the inventory id: ");
+        for (Item item : inventory){
+            System.out.println(id);
+            if (item.getId() == id){
+                inventory.remove(item);
+                System.out.println("Successful Delete: " + item);
+                Input.getLine("Press enter to continue...");
+                return;
+            }
         }
-    }
-}
+        System.out.println("ERROR: Inventory ID:" + id + " NOT found!");
+    } // end of deleteItem method
+
+    /**
+     * Add a book to the Library's inventory. Allows the user to enter the book's
+     author and genre.
+     *
+     * @param title The item's title.
+     * @param dateReceived The item's date received into inventory.
+     * @param description The item's description which is not a required field.
+     * @return The newly created book.
+     * @throws Exception Any exception thrown to addBook will be back to the
+    calling method.
+     */
+    private Book addBook(String title, String dateReceived, String description)
+            throws Exception {
+        Book book;
+        int userInput = 0;
+        String author = null;
+        BookGenre genre = null;
+        author = Input.getString("Author: ");
+        try {
+            userInput = Input.getIntRange("Genre 1=Children, 2=Fiction, 3=Nonfiction, 4=Mystery, 5=Scifi," +
+                    " 6=Romance: ", 1, 6);
+                    genre = BookGenre.values()[userInput - 1];
+        } catch (Exception e){
+            throw new Exception("Invalid data! Book Genre = " + genre);
+        }
+        book = new Book(genre, dateReceived, author, title);
+        book.setDesc(description);
+        return book;
+    } // end of addBook method
+
+
+    /**
+     * Add an item to inventory. Allow's the user to enter the item's title,
+     description,
+     * date received into inventory and then will call the appropriate item's type
+     (Book, CD, DVD) for additional
+     * user's input.
+     * @throws Exception All exceptions will be thrown back to the main menu for
+    handling.
+     */
+    private void addItem() throws Exception {
+        String title = null;
+        String description = null;
+        String dateReceived = null;
+        int inventoryType = 0;
+        System.out.println("Add Inventory");
+        System.out.println(SINGLE_LINE);
+        System.out.println("Please enter the following inventory information:");
+        title = Input.getString("Title: ");
+        dateReceived = Input.getDate("Date Received (MM-DD-YYYY): ");
+        description = Input.getLine("Description or press enter to continue: ");
+        inventoryType = Input.getIntRange("Type 1=Book, 2=CD, 3=DVD: ", 1, 3);
+        switch(inventoryType){
+            case 1:
+                Book book = addBook(title, dateReceived, description);
+                inventory.add(book);
+                System.out.println("Successful Add: " + book);
+                Input.getLine("Press enter to continue...");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                throw new Exception("Invalid Input! Inventory Type = " +
+                        inventoryType);
+        } // end of switch
+    } // end of addItem method
+    /**
+     * Display the Library's inventory's detail group by inventory type.
+     */
+    private void displayInventory(){
+        System.out.println("Book Inventory");
+        System.out.println(SINGLE_LINE);
+        System.out.println("ID  Title           Date Rec'd Author          Genre");
+        System.out.println("--- --------------- ---------- --------------- ----------");
+        for (Item item : inventory) {
+            if (item instanceof Book){
+                item.displayItem();
+            }
+        }
+        System.out.println();
+//TO-DO: ADD LOGIC FOR DISPLAYING OTHER INVENTORY TYPES
+        Input.getLine("Press enter to continue...");
+    } // end of displayInventory
+    /**
+     * The main menu that allows the user to add, delete, display, load, and save
+     inventory.
+     * @throws Exception All exceptions will be displayed or thrown back to the
+    main method.
+     */
+    private void mainMenu() throws Exception {
+        boolean keepRunning = true;
+        int userInput = 0;
+        while (keepRunning) {
+            System.out.println(SINGLE_LINE);
+            System.out.println("Main Menu");
+            System.out.println(SINGLE_LINE);
+            System.out.println("0 = End Game");
+            System.out.println("1 = Add Item");
+            System.out.println("2 = Delete Item");
+            System.out.println("3 = Display Inventory");
+            System.out.println("4 = Save Inventory");
+            System.out.println("5 = Load Inventory");
+            System.out.println(SINGLE_LINE);
+            userInput = Input.getIntRange("Menu Choice: ", 0, 6);
+            System.out.println(SINGLE_LINE);
+            switch (userInput) {
+                case 0:
+                    keepRunning = false;
+                    break;
+                case 1:
+                    try {
+                        this.addItem();
+                    } catch (Exception e){
+                        System.out.println(e.getMessage());
+                        Input.getLine("Press enter to continue...");
+                    }
+                    break;
+                case 2:
+                    try {
+                        this.deleteItem();
+                    } catch (Exception e){
+                        System.out.println(e.getMessage());
+                        Input.getLine("Press enter to continue...");
+                    }
+                    break;
+                case 3:
+                    displayInventory();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    throw new Exception("Invalid menu choice: " + userInput);
+            } // end of switch
+        } // end of while loop
+    } // end of mainMenu
+    /**
+     * Main method to keep the Library App running
+     * @param args Not used
+     * @throws Exception Any type of exceptions thrown back to main will be
+    displayed and end the program.
+     */
+    public static void main(String[] args) throws Exception {
+        LibraryApp app = new LibraryApp();
+        app.displayAppHeading();
+        try {
+            app.mainMenu();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Sorry but this program ended with an error. Please contact support!");
+        }
+        Input.sc.close();
+    } // end of main method
+} // end of LibraryApp class
